48d067dcf1fb51d0c42d7cccac5f68e4ce10da6f
AleksandrBerezin <berezin_s3@amil.ru>
2021-03-01 12:41:11 +0700

    feature(NoteApp): добавлена команды открытия окон AboutWindow и NoteWindow

---
+++ NoteAppWPF/NoteAppWPF/ViewModels/AboutViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace NoteAppWPF.ViewModels
+{
+    /// <summary>
+    /// Модель-представление окна <see cref="AboutWindow"/>
+    /// </summary>
+    public class AboutViewModel
+    {
+        public AboutViewModel()
+        {
+            var window = new AboutWindow();
+            window.ShowDialog();
+        }
+    }
+}
--- NoteAppWPF/NoteAppWPF/ViewModels/MainViewModel.cs
+++ NoteAppWPF/NoteAppWPF/ViewModels/MainViewModel.cs
@@ -5,22 +5,45 @@ using Core;
 
 namespace NoteAppWPF.ViewModels
 {
+    /// <summary>
+    /// Модель-представление окна <see cref="MainWindow"/>
+    /// </summary>
     public class MainViewModel : Notifier
     {
+        /// <summary>
+        /// Модель-представление окна редактирования заметки
+        /// </summary>
+        private NoteViewModel _noteViewModel;
+
+        /// <summary>
+        /// Модель-представление справочного окна
+        /// </summary>
+        private AboutViewModel _aboutViewModel;
+
         /// <summary>
         /// Проект
         /// </summary>
         private Project _project;
 
         /// <summary>
-        /// Список заметок, сортированный по дате изменения
-        /// </summary>
-        private List<Note> _currentDisplayedNotes;
-
-        /// <summary>
         /// Выбранная заметка
         /// </summary>
         private Note _selectedNote;
+
+        /// <summary>
+        /// Команда добавления новой заметки
+        /// </summary>
+        private RelayCommand _addNoteCommand;
+
+        /// <summary>
+        /// Команда редактировани заметки
+        /// </summary>
+        private RelayCommand _editNoteCommand;
+
+        /// <summary>
+        /// Команда открытия справочного окна
+        /// </summary>
+        private RelayCommand _openAboutWindowCommand;
 
         /// <summary>
         /// Возвращает и задает список заметок, сортированный по дате изменения
@@ -44,13 +67,65 @@ namespace NoteAppWPF.ViewModels
         /// <summary>
         /// Возвращает список категорий заметок
         /// </summary>
-        public List<string> NotesCategories { get; private set; }
+        public List<string> NoteCategories { get; private set; }
+
+        /// <summary>
+        /// Возвращает команду добавления новой заметки
+        /// </summary>
+        public RelayCommand AddNoteCommand
+        {
+            get
+            {
+                return _addNoteCommand ??
+                       (_addNoteCommand = new RelayCommand(obj =>
+                       {
+                           _noteViewModel = new NoteViewModel(null);
+                       }));
+            }
+        }
+
+        /// <summary>
+        /// Возвращает команду редактирования заметки
+        /// </summary>
+        public RelayCommand EditNoteCommand
+        {
+            get
+            {
+                return _editNoteCommand ??
+                       (_editNoteCommand = new RelayCommand(obj =>
+                       {
+                           if (SelectedNote == null)
+                           {
+                               return;
+                           }
+                           //TODO передавать копию
+                           _noteViewModel = new NoteViewModel(SelectedNote);
+
+                           ProjectManager.SaveToFile(_project, ProjectManager.DefaultPath);
+                       }));
+            }
+        }
+
+        /// <summary>
+        /// Возвращает команду открытия справочного окна
+        /// </summary>
+        public RelayCommand OpenAboutWindowCommand
+        {
+            get
+            {
+                return _openAboutWindowCommand ??
+                       (_openAboutWindowCommand = new RelayCommand(obj =>
+                       {
+                           _aboutViewModel = new AboutViewModel();
+                       }));
+            }
+        }
 
         public MainViewModel()
         {
             _project = ProjectManager.LoadFromFile(ProjectManager.DefaultPath);
-            NotesCategories = Enum.GetNames(typeof(NoteCategory)).ToList();
-            NotesCategories.Add("All");
+            NoteCategories = Enum.GetNames(typeof(NoteCategory)).ToList();
+            NoteCategories.Add("All");
 
             CurrentDisplayedNotes = _project.LastChangeTimeSort();
             _project.Notes = CurrentDisplayedNotes;
